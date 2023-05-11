3dd56292ea337f9509d816eb54163405622196a4
adeelriasat <adeelriasat60@gmail.com>
2023-05-11 18:53:09 +0100

    naming convention fix

--- assignment-3.py
+++ assignment-3.py
@@ -15,41 +15,39 @@ import matplotlib.pyplot as plt
 import cluster_tools as ct
 
 
-def TransposeDataFrame(df, country):
+def TransposeDataFrame(df):
     """
     this method takes the pandas dataframe
-    along with the indicator name and country name 
-    and convert into transposed dataframe.
+    convert into transposed dataframe.
     """
-    # df_country = df.loc[df['Country Name'] == country]
-
     # transpose the dataframe
-    df_country = df.transpose()
+    df_t = df.transpose()
 
     # set first row as columns of transpose df
-    df_country.columns = df_country.iloc[0]
+    df_t.columns = df_t.iloc[0]
 
     # remove non numerical data from dataframe
-    df_country = df_country.iloc[4:]
+    df_t = df_t.iloc[4:]
     
     # rename the index column
-    df_country.index.name = 'Year'
+    df_t.index.name = 'Year'
 
     # clean the transposed dataframe those are completely empty and then drop column with missing entries
-    df_country = df_country.dropna(how='all').dropna(axis=1)
+    df_t = df_t.dropna(how='all').dropna(axis=1)
 
     # make year as integer for the graph to avoid cluttering
-    df_country.index = pd.to_numeric(df_country.index)
+    df_t.index = pd.to_numeric(df_t.index)
 
-    return df_country
+    return df_t
 
 # read data using pandas and skip reference rows
 df_co2 = pd.read_csv('API_EN.ATM.CO2E.PC_DS2_en_csv_v2_5455265.csv', skiprows=4)
 
+# transpose data frame and clean empty rows
+df_co2_t = TransposeDataFrame(df_co2)
 
 # get get data for united kingdom
-df_co2_t = TransposeDataFrame(df_co2, 'United Kingdom')
-print(df_co2_t)
+print(df_co2_t.to_csv('test.csv'))
 # get get data for usa
 # df_uk = TransposeDataFrame(df_co2, 'United Kingdom')
 
